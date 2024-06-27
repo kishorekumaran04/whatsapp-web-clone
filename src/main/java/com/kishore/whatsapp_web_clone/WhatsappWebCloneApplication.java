b08606939ca95d5b445ea8fe8e8f00b251a10197
Kishore Kumaran <kishore.kumaran04@gmail.com>
2024-06-27 00:22:21 -0400

    first commit

---
+++ src/main/java/com/kishore/whatsapp_web_clone/WhatsappWebCloneApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.whatsapp_web_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappWebCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappWebCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kishore/whatsapp_web_clone/WhatsappWebCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.whatsapp_web_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappWebCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
